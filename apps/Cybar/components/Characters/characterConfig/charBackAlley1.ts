17d9450d24edab3665ad2d083ae13fad641ad099
Thomas Schmahl <thomas.schmahl@zendri.com>
2024-09-07 11:59:04 +0200

    feat: character dialogs

--- apps/Cybar/components/Characters/characterConfig/charBackAlley1.ts
+++ apps/Cybar/components/Characters/characterConfig/charBackAlley1.ts
@@ -6,4 +6,21 @@ export const charBackAlley1: CharacterProps = {
   left: "calc(45vw - 370px)",
   bottom: "120px",
   height: "240px",
+  dialogs: [
+    {
+      id: 0,
+      text: "Hey buddy, first timer ey. This place is such a rathole. Why I am still here? Debts, bud. Plus Debbie is so hoooot - damn",
+      nextDialog: 1,
+    },
+    {
+      id: 1,
+      text: "Think about investing first. If you're unsure, better set your brainthoughts broadcast to binance academy first.",
+      nextDialog: 2,
+    },
+    {
+      id: 2,
+      text: "If all other things fail, there's an Amazon suicide cell behind the next corner!But if you consider that, you can reach over your cybars to me first haha jk",
+      endsDialog: true,
+    },
+  ],
 };
--- apps/Cybar/components/Characters/characterConfig/index.ts
+++ apps/Cybar/components/Characters/characterConfig/index.ts
@@ -1,8 +1,20 @@
+import { DispatchAction } from "components/BackAlley/characterReducer";
 import { CharacterId } from "../characterIds.type";
 import { charBackAlley1 } from "./charBackAlley1";
 import { charBackAlley2 } from "./charBackAlley2";
 import { charBackAlley3 } from "./charBackAlley3";
 
+interface DialogContent {
+  text: string;
+  nextDialog?: number;
+  endsDialog?: boolean;
+  action?: DispatchAction;
+}
+
+interface Dialog extends DialogContent {
+  id: number;
+  answers?: DialogContent[];
+}
 export interface CharacterProps {
   id: CharacterId;
   name: string;
@@ -12,6 +24,7 @@ export interface CharacterProps {
   focusedBottom?: string;
   focusedLeft?: string;
   focusedHeight?: string;
+  dialogs: Dialog[];
 }
 
 export const BackAlleyChars = [charBackAlley1, charBackAlley2, charBackAlley3];
