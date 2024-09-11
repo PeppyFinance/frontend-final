e87718ed22170be82d3dc33d84ef26f6fd87c740
Thomas Schmahl <thomas.schmahl@zendri.com>
2024-09-11 11:42:29 +0200

    local changes

---
+++ apps/Cybar/components/BackAlley/characterContext.tsx
@@ -0,0 +1,51 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  useContext,
+  useReducer,
+} from "react";
+import {
+  CharacterState,
+  DispatchAction,
+  characterReducer,
+} from "./characterReducer";
+
+export interface CharacterContextValue {
+  characterState: CharacterState;
+  characterDispatch: Dispatch<DispatchAction>;
+}
+
+// TODO: refactor to remove undefined
+const CharacterContext = createContext<CharacterContextValue | undefined>(
+  undefined
+);
+interface Props {
+  children: ReactNode;
+}
+export const CharacterContextProvider = ({ children }: Props) => {
+  const [characterState, characterDispatch] = useReducer(characterReducer, {
+    dialog: undefined,
+    character: undefined,
+  });
+
+  const contextValue: CharacterContextValue = {
+    characterState,
+    characterDispatch,
+  };
+  return (
+    <CharacterContext.Provider value={contextValue}>
+      {children}
+    </CharacterContext.Provider>
+  );
+};
+
+export const useCharacterContext = () => {
+  const context = useContext(CharacterContext);
+  if (!context) {
+    throw new Error(
+      "DialogContext must be used within a DialogContextProvider"
+    );
+  }
+  return context;
+};
