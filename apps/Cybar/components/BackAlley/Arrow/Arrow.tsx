c8ba2034e359a164c500981ba282de9538994ea9
Thomas Schmahl <thomas.schmahl@zendri.com>
2024-09-18 15:51:37 +0200

    fix: arrow svg stroke

--- apps/Cybar/components/BackAlley/Arrow/Arrow.tsx
+++ apps/Cybar/components/BackAlley/Arrow/Arrow.tsx
@@ -55,15 +55,15 @@ const DoubleArrowIcon = () => {
         d="M5.5 5L11.7929 11.2929C12.1834 11.6834 12.1834 12.3166 11.7929 12.7071L5.5 19"
         strokeWidth="2"
         stroke={theme.characterAction}
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        strokeLinecap="square"
+        strokeLinejoin="miter"
       />
       <path
         d="M13.5 5L19.7929 11.2929C20.1834 11.6834 20.1834 12.3166 19.7929 12.7071L13.5 19"
         stroke={theme.characterAction}
         strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        strokeLinecap="square"
+        strokeLinejoin="miter"
       />
     </svg>
   );
