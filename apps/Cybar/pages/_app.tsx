1161cfa8b01275a63268069379131387abe30361
Thomas Schmahl <thomas.schmahl@zendri.com>
2025-01-12 14:50:49 +0100

    feat: merge

--- apps/Cybar/pages/_app.tsx
+++ apps/Cybar/pages/_app.tsx
@@ -1,18 +1,25 @@
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { setUseWhatChange } from "@simbathesailor/use-what-changed";
+import { BlockNumberProvider } from "@symmio/frontend-sdk/lib/hooks/useBlockNumber";
 import store, {
   ReduxProvider,
   persistor,
 } from "@symmio/frontend-sdk/state/declaration";
+import Updaters from "@symmio/frontend-sdk/state/updaters";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ErrorBoundary from "components/App/ErrorBoundaries";
+import Layout from "components/Layout";
+import { ModalBackground } from "components/Modal";
+import Popups from "components/Popups";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
 import { PersistGate } from "redux-persist/integration/react";
+import { ModalProvider } from "styled-react-modal";
+import ThemeProvider, { ThemedGlobalStyle } from "theme";
+import { getWagmiConfig } from "utils/wagmi";
 import { WagmiProvider } from "wagmi";
-// import dynamic from "next/dynamic";
-import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
-import { setUseWhatChange } from "@simbathesailor/use-what-changed";
-import { BlockNumberProvider } from "@symmio/frontend-sdk/lib/hooks/useBlockNumber";
-import Updaters from "@symmio/frontend-sdk/state/updaters";
-import ErrorBoundary from "components/App/ErrorBoundaries";
+import ConfigSDKComponent from "./configSDK";
 
 const { wagmiConfig, initialChain } = getWagmiConfig();
 export default function MyApp({ Component, pageProps }: AppProps) {
