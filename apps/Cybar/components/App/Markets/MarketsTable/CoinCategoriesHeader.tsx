ec91bc5be6ee1d3c3bce702e97ce8868c80bc4b4
Thomas Schmahl <thomas.schmahl@zendri.com>
2025-01-13 16:50:36 +0100

    wip: push category to router

--- apps/Cybar/components/App/Markets/MarketsTable/CoinCategoriesHeader.tsx
+++ apps/Cybar/components/App/Markets/MarketsTable/CoinCategoriesHeader.tsx
@@ -1,14 +1,24 @@
 import { useCoinCategories } from "@symmio/frontend-sdk/state/market/hooks";
 import { MarketsHeaderButton } from "components/Button";
+import { useRouter } from "next/router";
 import { HeaderWrap } from "./Header";
 
 export const CoinCategoriesHeader = () => {
+  const router = useRouter();
+
+  const onClick = (coinCategory: string) => {
+    // TODO: should direction be desc?
+    router.query.direction = "desc";
+    router.query.coinCategory = coinCategory;
+    router.push(router);
+  };
+
   const coinCategories = useCoinCategories();
 
   return (
     <HeaderWrap>
       {Object.entries(coinCategories).map(([key, _val]) => (
-        <MarketsHeaderButton isActive key={key}>
+        <MarketsHeaderButton isActive key={key} onClick={() => onClick(key)}>
           {key}
         </MarketsHeaderButton>
       ))}
