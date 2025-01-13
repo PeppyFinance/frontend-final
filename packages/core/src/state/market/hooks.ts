2d5c75860bb5e30b9a5a6f71ca14d22682587a7f
Thomas Schmahl <thomas.schmahl@zendri.com>
2025-01-13 16:06:10 +0100

    chore: format

--- packages/core/src/state/market/hooks.ts
+++ packages/core/src/state/market/hooks.ts
@@ -2,6 +2,8 @@ import { useAppSelector } from "../declaration";
 import { CoinCategories } from "./types";
 
 export function useCoinCategories(): CoinCategories {
-  const coinCategories: CoinCategories = useAppSelector((state) => state.market.coinCategories);
+  const coinCategories: CoinCategories = useAppSelector(
+    (state) => state.market.coinCategories,
+  );
   return coinCategories;
 }
