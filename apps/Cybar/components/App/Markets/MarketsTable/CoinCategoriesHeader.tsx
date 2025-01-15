95372ba27b70ed8b2fc238683e5e73a77e7da5d4
Thomas Schmahl <thomas.schmahl@zendri.com>
2025-01-15 17:22:32 +0100

    feat: categories button in markets

--- apps/Cybar/components/App/Markets/MarketsTable/CoinCategoriesHeader.tsx
+++ apps/Cybar/components/App/Markets/MarketsTable/CoinCategoriesHeader.tsx
@@ -1,8 +1,36 @@
 import { useCoinCategories } from "@symmio/frontend-sdk/state/market/hooks";
-import { MarketsHeaderButton } from "components/Button";
 import { useRouter } from "next/router";
-import { HeaderWrap } from "./Header";
+import styled from "styled-components";
 
+interface CategoryButton {
+  isActive: boolean;
+}
+const CategoryButton = styled.button<CategoryButton>`
+  width: 10em;
+  height: 40px;
+  position: relative;
+  text-align: center;
+  overflow: hidden;
+  background-color: ${({ isActive, theme }) =>
+    isActive ? theme.bg3 : theme.bg1};
+  color: ${({ isActive, theme }) => (isActive ? theme.text0 : theme.text4)};
+  border: 1px solid
+    ${({ isActive, theme }) => (isActive ? theme.text0 : theme.text4)};
+  &:hover {
+    background-color: ${({ theme }) => theme.bg3};
+    color: ${({ theme }) => theme.text1};
+    border-color: ${({ theme }) => theme.text1};
+  }
+`;
+
+const HeaderWrap = styled.div`
+  display: flex;
+  gap: 3em;
+  color: ${({ theme }) => theme.text4};
+  background-color: ${({ theme }) => theme.bg0};
+  padding: 24px 24px 15px 12px;
+  border-bottom: 1px solid ${({ theme }) => theme.border3};
+`;
 export const CoinCategoriesHeader = ({
   coinCategory,
 }: {
@@ -24,17 +52,17 @@ export const CoinCategoriesHeader = ({
 
   return (
     <HeaderWrap>
-      <MarketsHeaderButton isActive={!coinCategory} onClick={() => onClick()}>
+      <CategoryButton isActive={!coinCategory} onClick={() => onClick()}>
         All Coins
-      </MarketsHeaderButton>
+      </CategoryButton>
       {Object.entries(coinCategories).map(([key]) => (
-        <MarketsHeaderButton
+        <CategoryButton
           isActive={coinCategory?.toUpperCase() === key.toUpperCase()}
           key={key}
           onClick={() => onClick(key)}
         >
           {key}
-        </MarketsHeaderButton>
+        </CategoryButton>
       ))}
     </HeaderWrap>
   );
