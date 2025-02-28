4d374393da1db793f203e3c509506b2c0d6ff8ef
Thomas Schmahl <thmschmahl@gmail.com>
2025-02-28 16:48:51 +0100

    feat: character state implementations

---
+++ packages/core/src/state/character/character/index.ts
@@ -0,0 +1,6 @@
+import { charBackAlley1 } from "./characterBackAlley1";
+import { charBackAlley2 } from "./characterBackAlley2";
+import { charBackAlley3 } from "./characterBackAlley3";
+
+export const Characters = [charBackAlley1, charBackAlley2, charBackAlley3];
+
