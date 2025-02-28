712fe29e4f7abe60245a922547a13d4ad69ee99a
Thomas Schmahl <thmschmahl@gmail.com>
2025-02-28 16:48:01 +0100

    feat: setCharacter hook

---
+++ packages/core/src/state/character/hooks.ts
@@ -0,0 +1,12 @@
+import { useCallback } from "react";
+import { useAppDispatch, } from "../declaration";
+import { setCharaterActive } from "./actions";
+import { CharacterId } from "./types";
+
+export function useSetCharacter() {
+  const dispatch = useAppDispatch();
+
+  return useCallback((id: CharacterId) => {
+    dispatch(setCharaterActive(id))
+  }, [dispatch])
+}
